e3e84d937676ae9ec4102dfc97e354eb12b2ac58
Ryan Loos <RLoos289@gmail.com>
2016-08-26 12:47:51 -0700

    add 'add pizza' button

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 //<!-- Back End -->
 
 // <!--************pizza contstructor************-->
-function Pizza (size, topping) {
+function Pizza (size, topping, cost, ) {
   this.psize = size;
   this.ptopping = topping;
 }
@@ -23,16 +23,41 @@ Pizza.prototype.pizzaCost = function (psize, ptopping) {
   return cost;
 }
 
+function newPizza (size, topping) {
+  var pizzaNumber = 1;
+  pizzaNumber ++;
+  next_pizza = new Pizza (size, topping);
+  return next_pizza;
+}
+
 //<!-- Front End  -->
 $(document).ready(function(){
-  $('#submitButton').click(function(event){
+  $('#pizzaOptions').submit(function(event){
     event.preventDefault();
     var size = $('input[name=size]:checked').val();
     var topping = [];
     $('input[type=checkbox]:checked').each(function(i,e){topping.push( $(e).attr('value'))
-})
+      })
     pizza = new Pizza (size, topping);
     cost = pizza.pizzaCost();
+    $('#pizzaOptions')[0].reset();;
     console.log('Your pizza will cost $' + cost);
+    $('#pizzaList').append('<li>' + 'Pizza 1 $' + cost + '</li>')
+  });
+  $('#addPizza').click(function(event) {
+    event.preventDefault();
+    var pizzaArray = [pizza]
+
+    var size = $('input[name=size]:checked').val();
+    var topping = [];
+    $('input[type=checkbox]:checked').each(function(i,e){topping.push( $(e).attr('value'))
+      })
+    console.log(size,topping);
+    pizzaArray.push(newPizza(size,topping));
+    // newPizza(size, topping)
+    console.log('registered click');
+
+    debugger;
+    // next_pizza = new Pizza (size, topping);
   });
 });
